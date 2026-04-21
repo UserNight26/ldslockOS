299ef8da3c300d8c5118e716f6570112cc628479
Userlight <ldsnikola@gmail.com>
2026-04-21 12:17:04 -0300

    Create lock_screen.sh

---
+++ src/tools/lock_screen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ldslockOS Simple Lock Screen
+# This is much faster than Windows Lock
+
+# Background color (Solid black for 4GB RAM efficiency)
+i3lock -c 000000
+
+# Optional: Add a message in English
+# i3lock can be customized with images too!
